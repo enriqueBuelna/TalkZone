c2e61bd1e9feb3c362c680cd3a915db2dd860b9c
enriqueBuelna <jesusenriqueyt@gmail.com>
2024-12-11 20:29:47 -0700

    Ultimos commits

--- src/ui/pages/my-feed/choose-post/choose-post.component.ts
+++ src/ui/pages/my-feed/choose-post/choose-post.component.ts
@@ -1,18 +1,24 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { UserDemo } from '../../../../domain/models/user-demo.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-choose-post',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './choose-post.component.html',
-  styleUrl: './choose-post.component.css'
+  styleUrl: './choose-post.component.css',
 })
 export class ChoosePostComponent {
   @Output() clickEvent = new EventEmitter<boolean>(); // Evento de clic
-
+  rule = 'all';
   // Método para manejar el clic
-  onClick(option:boolean) {
-      this.clickEvent.emit(option);
+  onClick(option: boolean) {
+    if (option) {
+      this.rule = 'all';
+    } else {
+      this.rule = 'friends';
+    }
+    this.clickEvent.emit(option);
   }
 }
